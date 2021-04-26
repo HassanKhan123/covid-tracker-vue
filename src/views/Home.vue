0a9715cf6adbdae789b88d094056d391b7cac8e0
HassanKhan123 <hk2602809@gmail.com>
2021-04-26 16:29:08 +0500

    dataBoxes component added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,8 @@
 <template>
-  <main v-if="!loading"><DataTitle :text="title" :dataDate="dataDate" /></main>
+  <main v-if="!loading">
+    <DataTitle :text="title" :dataDate="dataDate" />
+    <DataBoxes :stats="stats" />
+  </main>
   <main v-else class="flex flex-col align-center justify-center text-center">
     <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Data</div>
     <img :src="loadingImage" class="w-24 m-auto" />
@@ -8,10 +11,13 @@
 
 <script>
 import DataTitle from "@/components/DataTitle";
+import DataBoxes from "@/components/DataBoxes";
+
 export default {
   name: "Home",
   components: {
     DataTitle,
+    DataBoxes,
   },
   data() {
     return {
