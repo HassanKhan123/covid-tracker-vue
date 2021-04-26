819f6c6da45461cc3b31c5487f02284f16977106
HassanKhan123 <hk2602809@gmail.com>
2021-04-26 16:01:30 +0500

    fetched data from api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,20 @@
 <template>
-  <div class="home">
-    Hello world
-  </div>
+  <div class="home">Hello world</div>
 </template>
 
 <script>
-
-
 export default {
-  name: 'Home',
-}
+  name: "Home",
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch("https://api.covid19api.com/summary");
+      const data = await res.json();
+      return data;
+    },
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+    console.log(data);
+  },
+};
 </script>
